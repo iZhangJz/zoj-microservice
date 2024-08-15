a73a1c817ed447e82713361e619f3689d91ced41
Zhang <2751628691@qq.com>
2024-08-15 11:18:10 +0800

    feat: 新增微服务网关 GateWay
    新增微服务网关 GateWay

--- zoj-backend-gateway/src/main/java/com/zjz/zojbackendgateway/ZojBackendGatewayApplication.java
+++ zoj-backend-gateway/src/main/java/com/zjz/zojbackendgateway/ZojBackendGatewayApplication.java
@@ -2,8 +2,11 @@ package com.zjz.zojbackendgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // 排除数据源自动配置
+@EnableDiscoveryClient
 public class ZojBackendGatewayApplication {
 
     public static void main(String[] args) {
