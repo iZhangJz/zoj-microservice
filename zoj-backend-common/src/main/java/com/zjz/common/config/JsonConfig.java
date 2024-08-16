2e689fd901ac606ccdcbb6a27bc17f8f17a1a9ea
Zhang <2751628691@qq.com>
2024-08-16 11:20:03 +0800

    feat: 新增全局配置
    1.新增 CorsConfig 跨域处理
    2.新增 GlobalAuthFilter 全局权限过滤器
    3.新增 JsonConfig Long 类型处理
    4.新增 MyBatisPlusConfig 分页逻辑处理

---
+++ zoj-backend-common/src/main/java/com/zjz/common/config/JsonConfig.java
@@ -0,0 +1,31 @@
+package com.zjz.common.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Spring MVC Json 配置
+ *
+ */
+@JsonComponent
+@ConditionalOnClass(ObjectMapper.class)
+public class JsonConfig {
+
+    /**
+     * 添加 Long 转 json 精度丢失的配置
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
